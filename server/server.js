4784c420265473cda1424860bd40f67e2a140d11
robert <robertrenbysanjuan@gmail.com>
2026-06-21 09:15:04 +0800

    save

--- server/server.js
+++ server/server.js
@@ -141,7 +141,7 @@ app.use((err, req, res, _next) => {
   return res.status(status).json(response)
 })
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 
 if (process.env.ENABLE_EMAIL_JOBS !== 'false') {
   startPaymentReminderJob()
